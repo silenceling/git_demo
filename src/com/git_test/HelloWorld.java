22679fff7168c56ce436e3f838cbc2461e9e31c3
silenceling <silencelg@hotmail.com>
2014-11-19 10:29:50 +0800

    git pull configuration test ok

--- src/com/git_test/HelloWorld.java
+++ src/com/git_test/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hi,git\n");
 		System.out.println("hi,helloworld!");
+		System.out.println("git pull is testing ok!");
 
 	}
 
